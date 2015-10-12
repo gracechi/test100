659135a7ad1bbcb9467cd4049229b92e78f9feeb
chen <yibeizi0925@163.com>
2015-10-13 07:56:07 +0800

    Initial Commit

---
+++ 10.13/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  10.13
+//
+//  Created by chen on 15/10/13.
+//  Copyright © 2015年 chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 10.13/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  10.13
+//
+//  Created by chen on 15/10/13.
+//  Copyright © 2015年 chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
